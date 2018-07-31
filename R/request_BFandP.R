3fb10f94ec946a74e9974806a0255cbeea2d578f
n-mounier <mounier.ninon@gmail.com>
2018-07-31 11:59:30 +0200

    Not using data.tables anymore + update pruning method

--- R/request_BFandP.R
+++ R/request_BFandP.R
@@ -45,15 +45,13 @@ request_BFandP <- function(Prior, sign_thresh, use_perm = F, save_files=F, verbo
   Log = update_log(Log, tmp, verbose)
 
   if(use_perm){# The true one : calculate BF
+    tmp = "   using a permutation approach: \n"
+    Log = update_log(Log, tmp, verbose)
+
+
 
     Prior = Prior[order(-Prior$BF),]
 
-
-
-
-    # true Z = just rs + log.bf
-
-    #trueZ[, !is.unsorted(-log.bf)] %|%stopifnot # should be sorted, why test again we just did it !!!
 
     # how many null z-scores should we simulate?
     nrow(Prior) -> N.one.set.of.nulls
@@ -129,193 +127,146 @@ request_BFandP <- function(Prior, sign_thresh, use_perm = F, save_files=F, verbo
     Log = update_log(Log, tmp, verbose)
 
 
-    Prior[, z := NULL]
-    Prior[, BF.null := NULL]
-   # Prior[, nullls_count := running.total]
+    Prior$z = NULL
+    Prior$BF.null = NULL
 
-    #stopifnot(max(running.total) <= full.number.of.nulls.for.comparison)
-    Prior[, BF_P := (running.total+1) / full.number.of.nulls.for.comparison ]
+    Prior$BF_P = (running.total+1) / full.number.of.nulls.for.comparison
 
 
   } else {
-
-  ### Functions to estimate p-value from BF/prior distribution
-
-  ## for zero prior (works with a vector of se2 -> returns a vector or p)
-  # function to calculate "Chi2" value, and return the p-value
-  # take variance : sigma**2 as input
-  zero_snp <- function(se2, BF){
-    p = pchisq(2 * (1+se2)/se2 * log(BF*sqrt(1+se2)), df=1, lower.tail=F)
-    return(p)
-  }
-
-  ## for non-zero prior (works with vectors of mu/sigma2 -> returns a vector or p)
-  # function to calculate x1 and x2, and return the p-value
-  # for SNP with non-zero prior (mu, sigma**2) - observed BF
-  nonzero_snp <- function(mu, sigma2, BF){
-    p = numeric(length(mu))
-
-    a = -1/(2*(sigma2+1)) + 1/2
-    b = mu/(sigma2+1)
-    c = -mu**2/(2*(sigma2+1)) - log(sqrt((sigma2+1))*BF)
-    delta = b**2 - 4*a*c
-
-    p[delta<0]=1
-
-    x1 = (-b[delta>0] - sqrt(delta[delta>0]))/(2*a[delta>0])
-    x2 = (-b [delta>0]+ sqrt(delta[delta>0]))/(2*a[delta>0])
-    p1 = pnorm(x1, lower.tail = T)
-    p2 = pnorm(x2, lower.tail = F)
-    p[delta>0] = p1+p2
-
-    return(p)
-  }
-
-  # calculate p-value for an observed BF, using formula from part 1 + part 2
-  # BF can be a vector of BFs
-  get_pValue <- function(BF, se_bychrm, zero_bychrm, non_zero){
-    res=data.frame(BF=BF)
-    res$my_p=NA
-    ntotal = sum(zero_bychrm) + nrow(non_zero)
-
-    # deal with all zero SNPs / deal with non-zero SNPs
-    res$my_p = apply(res, 1, function(x) ((sum(zero_snp(se_bychrm**2, x[1]) * zero_bychrm)
-                                           + sum(nonzero_snp(non_zero$prior_estimate, non_zero$prior_std_error**2, x[1])))) / ntotal)
-
-    return(res$my_p)
-  }
+    tmp = "   using a distribution approach: \n"
+    Log = update_log(Log, tmp, verbose)
 
 
-  # calculate p-value for an observed BF, using formula from part 1 only (zero prior SNPs)
-  # BF can be a vector of BFs
-  get_pValue_part1 <- function(BF, se_bychrm, zero_bychrm){
-    res=data.frame(BF=BF)
-    res$my_p=NA
-    ntotal = sum(zero_bychrm)
+    ### Functions to estimate p-value from BF/prior distribution
+    ## Calculate a p-value using the percentiles or any quantiles
+    get_PercentileP<- function(BF, perc, sigma2){ # BF : value -  perc : vector of quantiles - sigma2 : (variance) value
+      # here we need to suppress warnings because if BF small, delta<0, x1/x2 NA -> pb with p value
+      suppressWarnings({
+        a = pnorm( (-perc/sigma2) +
+                     sqrt(1+sigma2)/sigma2 *
+                     sqrt(perc**2 + 2*sigma2 * log(BF * sqrt(1+sigma2))), lower.tail=F) +
+          pnorm((-perc/sigma2) -
+                  sqrt(1+sigma2)/sigma2 *
+                  sqrt(perc**2 + 2*sigma2 * log(BF * sqrt(1+sigma2))), lower.tail=T)
+      })
+      a[is.na(a)]=1 # if NA -> replace by 1, that means that the polynome has no real solution and Pr(allBF>BF)=1
+      p=mean(a) # here, we assume that the quantiles are linearly distributed, so we can use an unweighted mean
+      return(p)
+    }
 
-    # deal with all zero SNPs
-    res$my_p = apply(res, 1, function(x) sum(zero_snp(se_bychrm**2, x[1]) * zero_bychrm) / ntotal)
+    # Get all p-values using the percentiles or any quantiles (wrapper function for "apply")
+    get_PercentileAll<- function(BF, perc, sigma2){ # BF : vector of values value -  perc : vector of quantiles - sigma2 : (variance) value
+      res=data.frame(BF=BF)
+      res$my_p=NA
 
-    return(res$my_p)
-  }
+      res$my_p = apply(res, 1, function(x) get_PercentileP(x[1], perc, sigma2))
 
-
-  # calculate p-value for an observed BF, using formula from part 2 only (non-zero prior SNPs)
-  # BF can be a vector of BFs
-  get_pValue_part2 <- function(BF, non_zero){
-    res=data.frame(BF=BF)
-    res$my_p=NA
-    ntotal =  nrow(non_zero)
-
-    # deal with all zero SNPs / deal with non-zero SNPs
-    res$my_p =  apply(res, 1, function(x) sum(nonzero_snp(non_zero$prior_estimate, non_zero$prior_std_error**2, x[1])) / ntotal)
-
-    return(res$my_p)
-  }
-
-
-  ## Percentiles
-
-  # funtion to approximate the second part of the formula, using percentiles
-  # sigma_mu + the number of non-zero SNPs
-  get_app_part2 <- function(BF, perc, sigma2, n){
-    # here we need to suppress warnings because if BF small, delta<0, x1/x2 NA -> pb with p value
-    suppressWarnings({
-      a = pnorm( (-perc/sigma2) +
-                   sqrt(1+sigma2)/sigma2 *
-                   sqrt(perc**2 + 2*sigma2 * log(BF * sqrt(1+sigma2))), lower.tail=F)
-      + pnorm((-perc/sigma2) -
-                sqrt(1+sigma2)/sigma2 *
-                sqrt(perc**2 + 2*sigma2 * log(BF * sqrt(1+sigma2))), lower.tail=T)
-    })
-    a[is.na(a)]=1
-    p=n*mean(a)
-    return(p)
-  }
+      return(res$my_p)
+    }
 
 
-  get_pValue_percentiles <- function(BF, se_bychrm, zero_bychrm, non_zero){
-    res=data.frame(BF=BF)
-    res$my_p=NA
-    ntotal = sum(zero_bychrm) + nrow(non_zero)
-    n2 = nrow(non_zero)
-    perc_mu = quantile(non_zero$prior_estimate, probs = seq(0,1,0.01))
-    sigma2_mu = mean(non_zero$prior_std_error)
+    ## Calculate a p-value using all priors
+    get_fullP <- function(BF, Data){ # BF : value - Data : data.frame with prior_estimate and prior_std_error columns
+      suppressWarnings({
+        a = pnorm( (-Data$prior_estimate/(Data$prior_std_error**2)) +
+                     sqrt(1+Data$prior_std_error**2)/(Data$prior_std_error**2) *
+                     sqrt(Data$prior_estimate**2 + 2*Data$prior_std_error**2 * log(BF * sqrt(1+Data$prior_std_error**2))), lower.tail=F) +
+          pnorm((-Data$prior_estimate/(Data$prior_std_error**2)) -
+                  sqrt(1+Data$prior_std_error**2)/(Data$prior_std_error**2) *
+                  sqrt(Data$prior_estimate**2 + 2*Data$prior_std_error**2 * log(BF * sqrt(1+Data$prior_std_error**2))), lower.tail=T)
+      })
+      a[is.na(a)]=1 # if NA -> replace by 1, that means that the polynome has no real solution and Pr(allBF>BF)=1
+      p=mean(a) # here, we use all the prior values, so we can use an unweighted mean
+      return(p)
+    }
 
-    # deal with all zero SNPs / deal with non-zero SNPs
-    res$my_p = apply(res, 1, function(x) (sum(zero_snp(se_bychrm**2, x[1]) * zero_bychrm)
-                                          + get_app_part2(x[1], perc_mu, sigma2_mu, n2)) / ntotal)
+    # Get all p-values using all priors (wrapper function for "apply")
+    get_fullAll<- function(BF, Data){ # BF : vector of values - Data : data.frame with prior_estimate and prior_std_error columns
+      res=data.frame(BF=BF)
+      res$my_p=NA
 
-    return(res$my_p)
-  }
+      res$my_p = apply(res, 1, function(x) get_fullP(x[1], Data))
 
-
-  # calculate p-value for an observed BF, using an hybrid approach
-  # BF can be a vector of BFs
-  # for each window, use only part 1, then compare for the last SNP : part 1 vs full p value
-  # if "high" difference, compare for the last SNP : percentile vs full p value
-  # if ok, use the percentile approach for this window
-  # if still "high" difference, re-calculate all p values from this window using part 1 + part 2
-  # if any p of the window "close" to significance, also re-calculate
-  get_pValue_hybrid_final <-  function(BF, se_bychrm, zero_bychrm, non_zero,
-                                       size_window=1000, sign_thr=5e-8, near_sign=1, tolerance=0.05){
-
-    n_window = ceiling(length(BF)/size_window)
-    near_sign = -log10(near_sign * sign_thr)
-
-    if(!is.unsorted(-BF)) stop("BF should be ordered decreasingly!")
-
-    res=data.frame(BF=BF)
-    res$new_p=NA
-
-    # get the "zero-prior SNPs approximation"
-    res$new_p =  get_pValue_part1(res$BF, se_bychrm, zero_bychrm)
-
-    for(wind in 1:n_window){
-      min_w = (wind-1)*size_window+1
-      max_w = ifelse(wind!=n_window,  wind*size_window, nrow(res) )
+      return(res$my_p)
+    }
 
 
-      # are we close to our significance threshold ?
-      # if true, we should use the full formula to avoid false positive / false negative
-      if(any(-log10(abs(res$new_p[min_w:max_w]-sign_thr))>near_sign)){
-        res$new_p[min_w:max_w] = get_pValue(res$BF[min_w:max_w], se_bychrm, zero_bychrm, non_zero)
-      } else { # sanity check : is our approximation "too" different from the true value ?
-        # true p-value, standard to compare with
-        p2 = get_pValue(res$BF[max_w], se_bychrm, zero_bychrm, non_zero)
-        # tolerance for this window
-        my_tolerance = -log10(tolerance*p2)
-        if(-log10(abs(res$new_p[max_w]-p2))<my_tolerance){ # if true
-          # compare the "percentile approximation
-          p_perc = get_pValue_percentiles(res$BF[max_w], se_bychrm, zero_bychrm, non_zero)
-          # is our approximation "too" different from the true value ?
-          if(-log10(abs(p_perc-p2))<my_tolerance){ # if true
-            res$new_p[min_w:max_w] = get_pValue(res$BF[min_w:max_w], se_bychrm, zero_bychrm, non_zero)
-          } else { # if false
-            # use the "percentile approximation" to get the p-values for this window
-            res$new_p[min_w:max_w] = get_pValue_percentiles(res$BF[min_w:max_w], se_bychrm, zero_bychrm, non_zero)
+    # Get all p-values using an hybrid approach
+    get_pValue <-  function(BF, Data,
+                            size_window=100, sign_thr=5e-8){
+
+      n_window = ceiling(length(BF)/size_window)
+
+      if(is.unsorted(rev(BF))) stop("BF should be ordered decreasingly!")
+
+      res=data.frame(BF=BF)
+      res$new_p=NA
+
+      # get the "percentile approximation"
+      perc = quantile(Data$prior_estimate, probs = seq(0,1,0.01))
+      sigma2 = mean(Data$prior_std_error**2)
+
+      res$new_p =  get_PercentileAll(res$BF, perc, sigma2)
+
+      ### Different steps, for different versions ####
+      # use "full p near significance" (only for the delicate window)
+      # + more quantiles for smaller p (identify the number of quantiles needed to keep the right order)
+      start_quantile = 0
+      for(wind in 1:n_window){
+        print(wind)
+        min_w = (wind-1)*size_window+1
+        max_w = ifelse(wind!=n_window,  wind*size_window, nrow(res) )
+
+        # each time, test the first one and the last one
+        pmin = get_fullP(res$BF[min_w], Data)
+        if(wind ==1 && pmin>sign_thr){
+          break # no SNP significant
+        }
+        # need to check if the limit was between two windows
+        if(wind != 1 && pmin>sign_thr){ # all the significant SNPs were in the previous windows
+          start_quantile = min_w-1
+          # + check the previous window, if perc approach was ok or if we need to reestimate it
+          wind=wind-1
+          min_w = (wind-1)*size_window+1
+          max_w = ifelse(wind!=n_window,  wind*size_window, nrow(res) )
+          if(res$new_p[min_w]>sign_thr){
+            start_quantile = min_w-1
+            res$new_p[min_w:max_w] = get_fullAll(res[min_w:max_w,"BF"], Data)
           }
+          break
+        }
+        pmax =  get_fullP(res$BF[max_w], Data)
+        # 1 if all sign / if all non sign, 0 if discordant
+        trueP = sign((pmin-sign_thr) * (pmax-sign_thr))
+        if(trueP<1){
+          start_quantile=min_w-1
+          res$new_p[min_w:max_w] = get_fullAll(res[min_w:max_w,"BF"], Data)
+          break
         }
       }
+      if(start_quantile>0){
+        try_more_quantiles = T
+        p_larger = res$new_p[start_quantile+1]
+        nquant = 5000 # by 1000
+        while(try_more_quantiles == T){
+          nquant = nquant + 1000
+          print(nquant)
+          quant = quantile(Data$prior_estimate, probs = seq(0,1,1/nquant))
+          p_try = get_PercentileP(res$BF[start_quantile], quant, sigma2)
+          if(p_larger > p_try) try_more_quantiles = F
+        }
+        res$new_p[1:start_quantile] = get_PercentileAll(res[1:start_quantile,"BF"], quant, sigma2)
+      }
+      return(res$new_p)
     }
-    return(res$new_p)
-  }
 
-
-  non_zero = Prior[Prior$prior_estimate!=0,]
-  zero = Prior[Prior$prior_estimate==0,]
-  zero_bychrm = table(zero$chrm)
-  se_bychrm = numeric(length = 22)
-  names(se_bychrm) = c(1:22)
-  se_bychrm = zero$prior_std_error[match(names(se_bychrm), zero$chrm)]
-
-
-
-  Prior$BF_P = get_pValue_hybrid_final(Prior$BF,
-                                       se_bychrm = se_bychrm, zero_bychrm = zero_bychrm, non_zero = non_zero,
-                                       size_window = 1000, sign_thr = sign_thresh, tolerance=0.05, near_sign = 1)
+    Prior = Prior[order(-Prior$BF),]
+    Prior$BF_P = get_pValue(Prior$BF, Prior,
+                            size_window = 100, sign_thr = sign_thresh)
 
   }
+
   if(save_files){
     system("rm Prior.csv")
     readr::write_csv(Prior, "PriorBFp.csv")
